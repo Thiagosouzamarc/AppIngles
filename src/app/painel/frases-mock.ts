d62993d7039bfed93f31543b99a8f4a3406dd914
Thiago Souza Marciano <thiagomarciano@brq.com>
2019-04-12 10:22:52 -0300

    seco

---
+++ src/app/painel/frases-mock.ts
@@ -0,0 +1,9 @@
+import {Frase} from '../shared/frase.model'
+
+
+export const FRASES: Frase[] = [
+    { fraseEng: 'I like to learn' ,  frasePtBr:'Eu gosto de aprender', frasePt: 'eu gosto de aprender' },
+    { fraseEng: 'I watch tv' ,  frasePtBr:'Eu assisto tv', frasePt: 'eu assisto tv'  },
+    { fraseEng: 'How are you?' ,  frasePtBr:'Como vai você?', frasePt: 'como vai você?'  },
+    { fraseEng: 'I eat bread' ,  frasePtBr:'Eu como pão', frasePt: 'eu como pão'  }
+]
